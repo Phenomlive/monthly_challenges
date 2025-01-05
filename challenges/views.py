f236104b37da839c258e5dfb9e8cc16405b23a5b
Fuad <fuadreddhow@gmail.com>
2025-01-06 00:25:17 +0600

    added usecase for monthly_challenge_by_number

--- challenges/views.py
+++ challenges/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 # Create your views here.
 
@@ -26,4 +26,9 @@ def monthly_challenge(request, month):
         return HttpResponseNotFound("This month is not supported!")
 
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+    if month > len(months):
+        return HttpResponseNotFound("Invalid month")
+    
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect(f"/challenges/{redirect_month}")
