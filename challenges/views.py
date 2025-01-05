2bbe21760fd69426c723785c651396c46e7eb3b2
Fuad <fuadreddhow@gmail.com>
2025-01-06 00:59:56 +0600

    created basic html structure

--- challenges/views.py
+++ challenges/views.py
@@ -4,6 +4,17 @@ from django.urls import reverse
 
 # Create your views here.
 
+def index(request):
+    months = list(monthly_challenges.keys())
+    list_items = ""
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+
+    response_data = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_data)
+
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
     "february": "Walk for at least 20 minutes every day!",
@@ -22,14 +33,15 @@ monthly_challenges = {
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        response_text = f"<h1>{challenge_text}</h1>"
+        return HttpResponse(response_text)
     except:
-        return HttpResponseNotFound("This month is not supported!")
+        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     if month > len(months):
-        return HttpResponseNotFound("Invalid month")
+        return HttpResponseNotFound("<h1>Invalid month</h1>")
     
     redirect_month = months[month - 1]
     redirect_path = reverse("month-challenge", args=[redirect_month])
