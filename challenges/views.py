9acb84446d64463a4e11726ad156e43fac7064b9
Fuad <fuadreddhow@gmail.com>
2025-01-06 10:56:21 +0600

    final commit

--- challenges/views.py
+++ challenges/views.py
@@ -44,3 +44,4 @@ def monthly_challenge_by_number(request, month):
     redirect_month = months[month - 1]
     redirect_path = reverse("month-challenge", args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
+
